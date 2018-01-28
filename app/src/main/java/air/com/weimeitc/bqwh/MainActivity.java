afadc740492b943c577b1df3e6597007fac8b9e5
youjian.wu <youjian.wu@qunar.com>
2018-01-28 22:06:07 +0800

    测试工程
    测试工程

---
+++ app/src/main/java/air/com/weimeitc/bqwh/MainActivity.java
@@ -0,0 +1,15 @@
+package air.com.weimeitc.bqwh;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import air.com.weimeitc.bqdj.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/air/com/weimeitc/bqwx/MainActivity.java
+++ app/src/main/java/air/com/weimeitc/bqwx/MainActivity.java
@@ -1,7 +1,9 @@
-package air.com.weimeitc.wmaneproject;
+package air.com.weimeitc.bqwx;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import air.com.weimeitc.bqdj.R;
 
 public class MainActivity extends AppCompatActivity {
 
